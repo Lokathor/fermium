f029b8a7b0e2e126a9f595ae5f31d3ba0ebc36ad
Lokathor <zefria@gmail.com>
2019-04-10 21:45:09 -0600

    suppress spurous warnings

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// This is bindgen's fault: https://github.com/rust-lang/rust-bindgen/issues/1549
+#![allow(improper_ctypes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
