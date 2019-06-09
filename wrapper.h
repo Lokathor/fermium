4b5111fe6c465af1dfc9299964230be2b1162474
Lokathor <zefria@gmail.com>
2019-06-09 02:19:01 -0600

    wrapper fixes, with CI fix

--- wrapper.h
+++ wrapper.h
@@ -2,11 +2,11 @@
 #define MAC_OS_X_VERSION_MIN_REQUIRED 1060
 #endif /* __APPLE__ */
 
-#include "include/SDL.h"
-
-// Note(Lokathor): It seems like we need to explicitly try turning on the video
-// driver flags here, *probably* because my "linux dev machine" is a virtual
-// server with no video system installed.
+/*
+Note(Lokathor): As a sanity check against the fact that I usually generate the
+linux bindings on a headless linux server, we will try to forcibly turn on the
+X11 and wayland driver stuff.
+*/
 #if defined(__unix__)
 
 #ifndef SDL_VIDEO_DRIVER_X11
@@ -18,5 +18,7 @@
 #endif /* SDL_VIDEO_DRIVER_WAYLAND */
 
 #endif /* __unix__ */
+
+#include "include/SDL.h"
 
 #include "include/SDL_syswm.h"
