e23c41e1f85412365976ed5e816edf0732e33881
Lokathor <zefria@gmail.com>
2020-12-13 20:39:13 -0700

    CI for binary link check.

--- build.rs
+++ build.rs
@@ -10,7 +10,6 @@ fn main() {
     let mut cm = cmake::Config::new(manifest_dir.join("SDL2-2.0.12"));
     cm.static_crt(true);
     cm.target(&env::var("TARGET").expect("Couldn't read `TARGET`"));
-
     if cfg!(feature = "dynamic_link") {
       cm.define("SDL_SHARED", "ON");
       cm.define("SDL_STATIC", "OFF");
@@ -18,7 +17,30 @@ fn main() {
       cm.define("SDL_SHARED", "OFF");
       cm.define("SDL_STATIC", "ON");
     }
+    let build_output_path = cm.build();
 
-    cm.build();
+    println!(
+      "cargo:rustc-link-search={}",
+      build_output_path.join("lib").display()
+    );
+
+    if cfg!(feature = "dynamic_link") {
+      println!("cargo:rustc-link-lib=SDL2");
+    } else {
+      println!("cargo:rustc-link-lib=static=SDL2d");
+      if cfg!(windows) {
+        println!("cargo:rustc-link-lib=user32");
+        println!("cargo:rustc-link-lib=gdi32");
+        println!("cargo:rustc-link-lib=winmm");
+        println!("cargo:rustc-link-lib=imm32");
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=oleaut32");
+        println!("cargo:rustc-link-lib=version");
+        println!("cargo:rustc-link-lib=uuid");
+        println!("cargo:rustc-link-lib=advapi32");
+        println!("cargo:rustc-link-lib=setupapi");
+        println!("cargo:rustc-link-lib=shell32");
+      }
+    }
   }
 }
