3363a76a5072c4329339246684f6847e41fdda74
Lokathor <zefria@gmail.com>
2020-12-21 20:13:31 -0700

    update error module

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,20 @@
 //! SDL uses a thread-local buffer to store the current error string.
 
-use crate::c_char;
+use crate::{c_char, c_int};
 
 extern "C" {
-  /// Gets the current error string, stored in a thread local buffer.
+  /// Get the last error message that was set for the current thread.
   ///
-  /// This function can be used even without having called [`SDL_Init`].
-  pub fn SDL_GetError() -> *const c_char;
+  /// SDL API functions may set error messages and then succeed, so you should
+  /// only use the error value if a function fails.
+  ///
+  /// * `errstr` A buffer to fill with the last error message that was set for
+  ///   the current thread.
+  /// * `maxlen` The size of the buffer pointed to by the `errstr` parameter.
+  ///
+  /// **Returns:** `errstr`
+  pub fn SDL_GetErrorMsg(errstr: *mut c_char, maxlen: c_int) -> *mut c_char;
 
-  /// Clears the thread-local current error buffer.
+  /// Clear the error message for the current thread.
   pub fn SDL_ClearError();
 }
